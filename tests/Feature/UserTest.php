442fe87bf143c8c1e7059ff3750783bf70431543
Mattia Filosa <mattia.filosa@hibo.it>
2024-12-04 10:04:32 +0100

    add check on user exams

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Exam;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
@@ -31,14 +32,34 @@ class UserTest extends TestCase
 
     public function test_authenticated_user_can_access_exams()
     {
+        $exam1 = Exam::factory()->create([
+            'title' => 'Esame di Matematica',
+            'date' => '2024-12-01',
+        ]);
+
+        $exam2 = Exam::factory()->create([
+            'title' => 'Esame di Fisica',
+            'date' => '2024-12-10',
+        ]);
+
+        $exam3 = Exam::factory()->create([
+            'title' => 'Esame di Chimica',
+            'date' => '2024-12-05',
+        ]);
+
         // create user
         $user = User::factory()->create();
+
+        $user->exams()->attach($exam1->id);
+        $user->exams()->attach($exam2->id);
+        $user->exams()->attach($exam3->id);
 
         // make get request to your exams
         $response = $this->actingAs($user, 'sanctum')->get('api/exams/user');
 
         // check response
         $response->assertStatus(200);
+        $response->assertJsonCount(3);
     }
 
     public function test_user_cannot_create_exam()
